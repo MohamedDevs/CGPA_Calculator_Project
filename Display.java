d21dc0b4b86fe192eb6fc7481050348b14436307
Mohamed Inzaman Hussain <159862647+MohamedDevs@users.noreply.github.com>
2024-03-02 07:21:01 +0530

    --Instruction Display Code--

---
+++ Display.java
@@ -0,0 +1,14 @@
+public class Display {
+    static public void displayInstruction() {
+        System.out.println("\n*Only Grades scored in Part-III papers are taken into consideration");
+        System.out.println("\nGrade\t\tGrade Points");
+        System.out.println("O\t\t\t10");
+        System.out.println("A+\t\t\t9");
+        System.out.println("A\t\t\t8");
+        System.out.println("B+\t\t\t7");
+        System.out.println("B\t\t\t6");
+        System.out.println("C\t\t\t5");
+        System.out.println("AB\t\t\t-");
+        System.out.println("RA\t\t\t-");
+    }
+}
